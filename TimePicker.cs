7c49ae960c8bfb77859d1f019b2d1cb407e0aaa2
imoimo2009 <k_imanishi@tec.tomakomai.or.jp>
2022-06-30 11:28:46 +0900

    パラメータにコメントを追加_(:3」∠)_

--- TimePicker.cs
+++ TimePicker.cs
@@ -10,107 +10,108 @@ namespace TimePicker
         // 時間・分モード切替用列挙体
         enum eMode
         {
-            Hour,
-            Minute
+            Hour,       // 時間入力モード
+            Minute      // 分入力モード
         }
 
         // ブラシインデックスの列挙体
         enum eBrush
         {
-            BG,
-            BASE,
-            CELL,
-            SCELL,
-            RCELL,
-            CLOSE,
-            SCLOSE
+            BG,         // バックグラウンド
+            BASE,       // 時計盤
+            CELL,       // 時間表示円
+            SCELL,      // マウスオーバー時
+            RCELL,      // 選択項目
+            CLOSE,      // 閉じるボタン
+            SCLOSE      // 閉じるボタンマウスオーバー時
         }
 
         // ペンインデックスの列挙体
         enum ePen
         {
-            SLINE,
-            RLINE,
-            CLOSE,
-            SCLOSE
+            SLINE,      // マウスオーバー時
+            RLINE,      // 選択項目
+            CLOSE,      // 閉じるボタン
+            SCLOSE      // 閉じるボタンマウスオーバー時
         }
 
         // 選択カーソルパラメータインデックスの列挙体
         enum eCell
         {
-            Radian,
-            Radius,
-            Size,
-            Font,
-            Brush,
-            Pen,
-            Value
+            Radian,     // 角度（ラジアン）
+            Radius,     // 時計盤原点からの半径
+            Size,       // サイズ
+            Font,       // フォント
+            Brush,      // ブラシ
+            Pen,        // ペン
+            Value       // 表示内容
         }
 
         // 定数定義
-        private const int DefaultWidth = 200;
-        private const int DefaultHeight = 240;
-        private const int BitmapWidth = 800;
-        private const int BitmapHeight = 960;
-        private const int CenterLeft = 400;
-        private const int CenterTop = 560;
-        private const int BGRadius = 32;
-        private const int BGRadiusx2 = BGRadius * 2;
-        private const int BGLeftTopDeg = 180;
-        private const int BGRightTopDeg = 270;
-        private const int BGLeftBottomDeg = 90;
-        private const int BGRrightBottomDeg = 0;
-        private const int DigitalLeft = 200;
-        private const int DigitalTop = 40;
-        private const int DigitalWidth = 400;
-        private const int DigitalHeight = 144;
-        private const int CloseBtnLeft = BitmapWidth - 37;
-        private const int CloseBtnTop = 37;
-        private const int CloseBtnRadius = 27;
-        private const int CloseBtnLine = 15;
-        private const int CloseBtnLineWidth = 6;
-        private const int BaseSize = 720;
-        private const int ValueSize = 100;
-        private const int ValueRadius = 292;
-        private const int ValueSize2 = 88;
-        private const int ValueRadius2 = 188;
-        private const string ClkFontName = "ＭＳ　ゴシック";
-        private const int ClkFontSise = 48;
-        private const int ClkFontSise2 = 36;
-        private const string DigitalFontName = "ＭＳ　ゴシック";
-        private const int DigitalFontSise = 80;
-        private const int LineWidth = 8;
-        private const string ClkFormat = "D2";
-        private const string BrushesColor_BG = "#AAAAFF";
-        private const string BrushesColor_BASE = "#4444AA";
-        private const string BrushesColor_CELL = "#6666CC";
-        private const string BrushesColor_SCELL = "#FFFFFF";
-        private const string BrushesColor_RCELL = "#AAAADD";
-        private const string BrushesColor_CLOSE = "#FFAAAA";
-        private const string BrushesColor_SCLOSE = "#FF0000";
-        private const string PensColor_SLINE = "#FFFFFF";
-        private const string PensColor_RLINE = "#AAAADD";
-        private const string PensColor_CLOSE = "#FF0000";
-        private const string PensColor_SCLOSE = "#FFDDDD";
+        private const int DefaultWidth = 200;                       // デフォルトの幅
+        private const int DefaultHeight = 240;                      // デフォルトの高さ
+        private const int BitmapWidth = 800;                        // ビットマップの幅
+        private const int BitmapHeight = 960;                       // ビットマップの高さ
+        private const int CenterLeft = 400;                         // 時計盤の原点X
+        private const int CenterTop = 560;                          // 時計盤の原点Y
+        private const int BGRadius = 32;                            // 背景角丸の半径
+        private const int BGDiameter = BGRadius * 2;                // 背景角丸の直径
+        private const int BGLeftTopDeg = 180;                       // 左上の開始角
+        private const int BGRightTopDeg = 270;                      // 右上の開始角
+        private const int BGLeftBottomDeg = 90;                     // 左下の開始角
+        private const int BGRrightBottomDeg = 0;                    // 右下の開始角
+        private const int CloseBtnLeft = BitmapWidth - 37;          // 閉じるボタンの位置X
+        private const int CloseBtnTop = 37;                         // 閉じるボタンの位置Y
+        private const int CloseBtnRadius = 27;                      // 閉じるボタンの半径
+        private const int CloseBtnLine = 15;                        // 閉じるボタンＸの描画開始位置
+        private const int CloseBtnLineWidth = 6;                    // 閉じるボタンＸの太さ
+        private const int BaseRadius = 360;                         // 時計盤の半径
+        private const int BaseDiameter = BaseRadius * 2;            // 時計盤の直径
+        private const int ValueSize = 50;                           // 時間表示のサイズ(外側)
+        private const int ValueRadius = 292;                        // 時間表示の原点からの距離(外側)
+        private const int ValueSize2 = 44;                          // 時間表示のサイズ(内側)
+        private const int ValueRadius2 = 188;                       // 時間表示の原点からの距離(内側)
+        private const string ClkFontName = "ＭＳ　ゴシック";        // 時間表示のフォント名称 
+        private const int ClkFontSise = 48;                         // 時間表示のフォントサイズ(外側)
+        private const int ClkFontSise2 = 36;                        // 時間表示のフォントサイズ(内側)
+        private const int ClkLineWidth = 8;                         // 時計盤の針の太さ
+        private const string ClkFormat = "D2";                      // 時計盤のフォーマット文字列
+        private const int DigitalLeft = 200;                        // デジタル矩形部の位置X
+        private const int DigitalTop = 40;                          // デジタル矩形部の位置Y
+        private const int DigitalWidth = 400;                       // デジタル矩形部の幅
+        private const int DigitalHeight = 144;                      // デジタル矩形部の高さ
+        private const string DigitalFontName = "ＭＳ　ゴシック";    // デジタル表示部のフォント名称
+        private const int DigitalFontSise = 80;                     // デジタル表示部のフォントサイズ
+        private const string BrushesColor_BG = "#AAAAFF";           // 背景の色(ブラシ)
+        private const string BrushesColor_BASE = "#4444AA";         // 時計盤の色(ブラシ)
+        private const string BrushesColor_CELL = "#6666CC";         // 時間表示の色(ブラシ)
+        private const string BrushesColor_SCELL = "#FFFFFF";        // マウスオーバー時の色(ブラシ)
+        private const string BrushesColor_RCELL = "#AAAADD";        // 選択項目の色(ブラシ)
+        private const string BrushesColor_CLOSE = "#FFAAAA";        // 閉じるボタンの色(ブラシ)
+        private const string BrushesColor_SCLOSE = "#FF0000";       // 閉じるボタンマウスオーバーの色(ブラシ)
+        private const string PensColor_SLINE = "#FFFFFF";           // マウスオーバー時の色(ペン)
+        private const string PensColor_RLINE = "#AAAADD";           // 選択項目の色(ペン)
+        private const string PensColor_CLOSE = "#FF0000";           // 閉じるボタンの色(ペン)
+        private const string PensColor_SCLOSE = "#FFDDDD";          // 閉じるボタンマウスオーバーの色(ペン)
 
         // 変数定義
-        private int X,Y;
-        private bool Clicked;
-        private eMode Mode;
-        private PointF DrawScale;
-        private Point Center,CloseBtn;
-        private Rectangle DigitalRect;
-        private SolidBrush[] Brushes;
-        private Pen[] Pens;
-        private Bitmap Bmp;
-        private Graphics Gp;
-        private StringFormat Format;
-        private Font ClkFont,ClkFont2;
+        private int X,Y;                                            // マウス座標
+        private bool Clicked;                                       // クリックの状態
+        private eMode Mode;                                         // 入力モード
+        private PointF DrawScale;                                   // 描画スケール
+        private Point Center,CloseBtn;                              // 原点、閉じるボタンの位置
+        private Rectangle DigitalRect;                              // デジタル表示部の矩形
+        private SolidBrush[] Brushes;                               // ブラシ格納用
+        private Pen[] Pens;                                         // ペン格納用
+        private Bitmap Bmp;                                         // ビットマップ
+        private Graphics Gp;                                        // ビットマップ描画用グラフィックオブジェクト
+        private StringFormat Format;                                // 文字列配置指定
+        private Font ClkFont,ClkFont2;                              // 時間表示フォント(外側、内側)
 
         // プロパティ定義
-        public int Hour { get; private set; }
-        public int Minute { get; private set; }
-        public new string Text { get; set; }
+        public int Hour { get; private set; }                       // 時間
+        public int Minute { get; private set; }                     // 分
+        public new string Text { get; set; }                        // 時刻文字列
 
         // コンストラクタ
         public TimePicker() : base()
@@ -167,8 +168,8 @@ namespace TimePicker
                 new Pen(ColorTranslator.FromHtml(PensColor_CLOSE)),
                 new Pen(ColorTranslator.FromHtml(PensColor_SCLOSE))
             };
-            Pens[(int)ePen.SLINE].Width = LineWidth;
-            Pens[(int)ePen.RLINE].Width = LineWidth;
+            Pens[(int)ePen.SLINE].Width = ClkLineWidth;
+            Pens[(int)ePen.RLINE].Width = ClkLineWidth;
             Pens[(int)ePen.CLOSE].Width = CloseBtnLineWidth;
             Pens[(int)ePen.SCLOSE].Width = CloseBtnLineWidth;
             Bmp = new Bitmap(BitmapWidth, BitmapHeight); // レンダリング用ビットマップオブジェクト
@@ -198,8 +199,8 @@ namespace TimePicker
         private void TimePicker_Paint(object sender, PaintEventArgs e)
         {
             // 変数定義
-            int r,s,f = BaseSize;
-            double rad,d;
+            int r,s,d;
+            double rad,dt;
             Font fnt;
             Point a,c = Center;
             Graphics g = Gp;
@@ -210,15 +211,15 @@ namespace TimePicker
             Rectangle dr = DigitalRect;
 
             // 背景
-            g.FillRectangle(b, 0,BGRadius, sz.Width, sz.Height - BGRadiusx2);
-            g.FillRectangle(b, BGRadius, 0, sz.Width - BGRadiusx2, sz.Height);
-            g.FillPie(b, 0, 0, BGRadiusx2, BGRadiusx2, BGLeftTopDeg, 90);
-            g.FillPie(b, sz.Width - BGRadiusx2, 0, BGRadiusx2, BGRadiusx2, BGRightTopDeg, 90);
-            g.FillPie(b, 0, sz.Height - BGRadiusx2, BGRadiusx2, BGRadiusx2, BGLeftBottomDeg, 90);
-            g.FillPie(b, sz.Width - BGRadiusx2, sz.Height - BGRadiusx2, BGRadiusx2, BGRadiusx2, BGRrightBottomDeg, 90);
+            g.FillRectangle(b, 0,BGRadius, sz.Width, sz.Height - BGDiameter);
+            g.FillRectangle(b, BGRadius, 0, sz.Width - BGDiameter, sz.Height);
+            g.FillPie(b, 0, 0, BGDiameter, BGDiameter, BGLeftTopDeg, 90);
+            g.FillPie(b, sz.Width - BGDiameter, 0, BGDiameter, BGDiameter, BGRightTopDeg, 90);
+            g.FillPie(b, 0, sz.Height - BGDiameter, BGDiameter, BGDiameter, BGLeftBottomDeg, 90);
+            g.FillPie(b, sz.Width - BGDiameter, sz.Height - BGDiameter, BGDiameter, BGDiameter, BGRrightBottomDeg, 90);
             // 閉じるボタン
-            d = GetDistance(X, Y,CloseBtn.X,CloseBtn.Y);
-            if (d <= CloseBtnRadius)
+            dt = GetDistance(X, Y,CloseBtn.X,CloseBtn.Y);
+            if (dt <= CloseBtnRadius)
             {
                 b = Brushes[(int)eBrush.SCLOSE];
                 p = Pens[(int)ePen.SCLOSE];
@@ -235,7 +236,9 @@ namespace TimePicker
             g.DrawLine(p, a.X - r, a.Y - r, a.X + r, a.Y + r);
             g.DrawLine(p, a.X - r, a.Y + r, a.X + r, a.Y - r);
             // アナログ部
-            g.FillPie(Brushes[(int)eBrush.BASE], c.X - f / 2, c.Y - f / 2, f, f, 0, 360);
+            r = BaseRadius;
+            d = BaseDiameter;
+            g.FillPie(Brushes[(int)eBrush.BASE], c.X - r, c.Y - r, d, d, 0, 360);
             if (Mode == 0) {
                 // 時間入力モード
                 for (int i = 23; i >= 0; i--)
@@ -256,8 +259,8 @@ namespace TimePicker
                     }
                     rad = Rad((i % 12) * 30 - 90); // 時計盤に表示する時間の角度を取得
                     a = GetArcPos(rad, r, c.X, c.Y); // 時計盤に表示する時間の座標を取得
-                    d = GetDistance(X, Y, a.X, a.Y); //マウスカーソルと時間の座標の距離を求める
-                    if (d <= s / 2) // 時間表示の円の内側にマウスカーソルがあるとき
+                    dt = GetDistance(X, Y, a.X, a.Y); //マウスカーソルと時間の座標の距離を求める
+                    if (dt <= s) // 時間表示の円の内側にマウスカーソルがあるとき
                     {
                         // 選択カーソルを追加（マウス追随）
                         cell.Add(new object[]
@@ -280,7 +283,7 @@ namespace TimePicker
                         });
                     }
                     // 時間の描画
-                    g.FillPie(Brushes[(int)eBrush.CELL], a.X - s / 2, a.Y - s / 2, s, s, 0, 360);
+                    g.FillPie(Brushes[(int)eBrush.CELL], a.X - s, a.Y - s, s * 2, s * 2, 0, 360);
                     g.DrawString(i.ToString(ClkFormat), fnt, Brushes[(int)eBrush.BASE], a.X, a.Y, Format);
                 }
                 // デジタル切り替え用ブラシを設定
@@ -304,8 +307,8 @@ namespace TimePicker
                     Rad(Minute * 6 - 90),ValueRadius,ValueSize,ClkFont,
                     Brushes[(int)eBrush.RCELL],Pens[(int)ePen.RLINE],Minute.ToString(ClkFormat)
                 });
-                d = GetDistance(X, Y, c.X, c.Y); // マウスカーソルと中心座標の距離を求める
-                if (d <= f / 2) // マウスカーソルが時計盤の中にあるとき
+                dt = GetDistance(X, Y, c.X, c.Y); // マウスカーソルと中心座標の距離を求める
+                if (dt <= BaseRadius) // マウスカーソルが時計盤の中にあるとき
                 {
                     rad = Math.Atan2(Y - c.Y, X - c.X); // 中心座標とマウスカーソルの相対角度を取得
                     int min = Rad2Minute(rad); // 角度(ラジアン）から分を取得
@@ -333,7 +336,7 @@ namespace TimePicker
                 g.DrawLine((Pen)o[(int)eCell.Pen], c.X, c.Y, a.X, a.Y);
                 g.FillPie((SolidBrush)o[(int)eCell.Brush], c.X - 16, c.Y - 16, 32, 32, 0, 360);
                 s = (int)o[(int)eCell.Size];
-                g.FillPie((SolidBrush)o[(int)eCell.Brush], a.X - s / 2, a.Y - s / 2, s, s, 0, 360);
+                g.FillPie((SolidBrush)o[(int)eCell.Brush], a.X - s, a.Y - s, s * 2, s * 2, 0, 360);
                 g.DrawString((string)o[(int)eCell.Value], (Font)o[(int)eCell.Font], Brushes[(int)eBrush.BASE], a.X, a.Y, Format);
             }
             // デジタル部
